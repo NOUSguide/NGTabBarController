cd9cd91a985f0f2da7669916f92c122f5e0fdc99
Juergen Falb <juergen@falb.at>
2017-01-06 22:28:25 +0100

    Added framework to project

---
+++ NGTabBarKit/NGTabBarKit.h
@@ -0,0 +1,26 @@
+//
+//  NGTabBarKit.h
+//  NGTabBarKit
+//
+//  Created by Juergen Falb on 06/01/2017.
+//  Copyright © 2017 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NGTabBarKit.
+FOUNDATION_EXPORT double NGTabBarKitVersionNumber;
+
+//! Project version string for NGTabBarKit.
+FOUNDATION_EXPORT const unsigned char NGTabBarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NGTabBarKit/PublicHeader.h>
+#import <NGTabBarKit/NGTabBarController.h>
+#import <NGTabBarKit/NGTabBarControllerDelegate.h>
+#import <NGTabBarKit/NGTabBar.h>
+#import <NGTabBarKit/NGTabBarControllerAnimation.h>
+#import <NGTabBarKit/NGTabBarPosition.h>
+#import <NGTabBarKit/NGTabBarItem.h>
+
+#import <NGTabBarKit/UIViewController+NGTabBarItem.h>
+#import <NGTabBarKit/UINavigationController+NGTabBarNavigationDelegate.h>
